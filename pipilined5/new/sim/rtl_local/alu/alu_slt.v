4e173f2e67343c12660b31a36b5d4617a8ecdfaf
ccycyc6 <1287315178@qq.com>
2026-07-14 17:23:45 +0800

    clean

--- pipilined5/new/sim/rtl_local/alu/alu_slt.v
+++
@@ -1,8 +0,0 @@
-module alu_slt(
-    input  wire          lt,
-    output wire  [31:0]  out
-);
-
-assign out = {31'b0, lt};
-
-endmodule
--- pipilined5/new/sim/rtl_local/alu/alu_xor.v
+++
@@ -1,9 +0,0 @@
-module alu_xor(
-    input  wire [31:0] a,
-    input  wire [31:0] b,
-    output wire [31:0] out
-);
-
-assign out = a ^ b;
-
-endmodule
